3f78591324e68ad22cf6b308feeb7832a9092d5b
Noel Hoppe <noel.hoppe@gmx.de>
2025-02-26 19:52:35 +0100

    add subset to lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,7 +3,8 @@ import {Lusitana} from "next/font/google";
 
 const inter = Inter({subsets: ['latin']});
 const lusitana = Lusitana({
-    weight : ['400', '700']
+    weight : ['400', '700'],
+    subsets: ["latin"]
 });
 
 export {
